c0951a59b95afc27f21150c67530796849a62cb7
saidm <said.mgdv@yandex.ru>
2025-07-23 22:04:43 +0300

    ласт

--- service/settings.py
+++ service/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'products.middleware.CheckSubscriptionMiddleware',
+    'products.middleware.CheckSubscriptionMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
